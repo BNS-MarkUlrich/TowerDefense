eed32fcad55644240a9852b1af20253e78f12067
Mark Ulrich <31822@ma-web.nl>
2021-11-06 02:55:18 +0100

    Added pause screen

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private GameObject onPauseGame;
+
     [Header("Look Sensitivity")]
     public float sensX;
     public float sensY;
@@ -23,21 +25,30 @@ public class CameraController : MonoBehaviour
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        isSpecatator = true;
+        Time.timeScale = 1;
     }
 
     private void Update()
     {
-        // get the mouse movement inputs
-        rotX += Input.GetAxis("Mouse X") * sensX;
-        rotY += Input.GetAxis("Mouse Y") * sensY;
-
-        // clam the vertical rotation
-        rotY = Mathf.Clamp(rotY, minY, maxY);
-
-        // are we spectator?
-        isSpecatator = true;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            onPauseGame.SetActive(true);
+            Time.timeScale = 0;
+            isSpecatator = false;
+            Cursor.lockState = CursorLockMode.None;
+        }
         if (isSpecatator)
         {
+            // get the mouse movement inputs
+            rotX += Input.GetAxis("Mouse X") * sensX;
+            rotY += Input.GetAxis("Mouse Y") * sensY;
+
+            // clam the vertical rotation
+            rotY = Mathf.Clamp(rotY, minY, maxY);
+
+            // are we spectator?
+
             // rotate cam vertically
             transform.rotation = Quaternion.Euler(-rotY, rotX, 0);
 
@@ -54,5 +65,13 @@ public class CameraController : MonoBehaviour
             Vector3 dir = transform.right * x + transform.up * y + transform.forward * z;
             transform.position += dir * spectatorMoveSpeed * Time.deltaTime;
         }
+    }
+
+    public void UnPauseGame()
+    {
+        onPauseGame.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        isSpecatator = true;
+        Time.timeScale = 1;
     }
 }
